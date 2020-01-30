8cdca9793dc7fe117f578374ff34290a3073ecc1
Elliot Wilson <eedub42@gmail.com>
2020-01-30 00:11:55 -0700

    Fix website title and scaling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { PdfViewer } from './components'
 import Headshot from './assets/headshot.jpg'
 import ResumePdf from './assets/pdfs/resume.pdf'
 import CareerChart from './assets/pdfs/career_chart.pdf'
 import './App.css'
+import { useWindowSize } from './utility/hooks'
+
+const defaultSizes = {
+	career: {
+		width: 960,
+		height: 540,
+	},
+	resume: {
+		width: 612,
+		height: 792,
+	},
+}
 
 function App() {
+	const size = useWindowSize()
+
+	const getResumeSize = useCallback(() => {
+		return getScale(size, defaultSizes.resume)
+	}, [size, size.width])
+
+	const getCareerChartSize = useCallback(() => {
+		return getScale(size, defaultSizes.career)
+	}, [size, size.width])
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={Headshot} className="App-logo" alt="logo" />
 			</header>
-			<PdfViewer file={ResumePdf} />
+			<PdfViewer file={ResumePdf} scale={getResumeSize()} />
 			<div className="break"></div>
-			<PdfViewer file={CareerChart} />
+			<PdfViewer file={CareerChart} scale={getCareerChartSize()} />
 		</div>
 	)
+}
+
+function getScale(size, defaultSize) {
+	const targetWidth = size.width - 40
+	const imageWidth = defaultSize.width
+	if (imageWidth < targetWidth) return 1
+	const scale = targetWidth / imageWidth
+	return scale
 }
 
 export default App
--- src/components/PdfViewer.js
+++ src/components/PdfViewer.js
@@ -1,10 +1,10 @@
-import React, { useState, useRef } from 'react'
+import React from 'react'
 import Pdf from '@mikecousins/react-pdf'
 
-export default function PdfViewer({ file }) {
+export default function PdfViewer({ file, ...rest }) {
 	return (
 		<div>
-			<Pdf file={file} page={1} />
+			<Pdf file={file} page={1} {...rest} />
 		</div>
 	)
 }
